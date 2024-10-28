1a51f4d3907c206e879232387f49b83295463067
Michael Amting <michaelamting@outlook.de>
2024-10-28 20:29:28 +0100

    Optimizing Update-Banner

--- omonsch-customer-backend.php
+++ omonsch-customer-backend.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Oliver Monschau - Customer Backend
 Description: Customizes the WordPress admin area for customers.
-Version: 0.1.5
+Version: 0.1.6
 Author: Oliver Monschau, Michael Amting
 */
 
